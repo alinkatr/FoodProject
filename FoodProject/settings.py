8356735ce49693fcc5afed3c38710f4a9cc5888a
Алина Трошкова <alinka7082006@mail.ru>
2026-01-10 16:38:03 +0500

    changing hosts

--- FoodProject/settings.py
+++ FoodProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4095b289-ba34-4e2a-ba88-64f7929fe180'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
