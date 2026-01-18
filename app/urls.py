4835bc7de77cf6aaf91883ea135cf3d3792a4d8e
Алина Трошкова <alinka7082006@mail.ru>
2026-01-18 17:40:19 +0500

    adding urls logic

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,17 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    
+    path('products/', views.product_list, name='product_list'),
+    path('products/add/', views.product_add, name='product_add'),
+    path('products/<int:pk>/edit/', views.product_edit, name='product_edit'),
+    path('products/<int:pk>/delete/', views.product_delete, name='product_delete'),
+    path('products/<int:pk>/mark_used/', views.product_mark_used, name='product_mark_used'),
+    path('products/statistics/', views.product_statistics, name='product_statistics'),
+    
+    path('recommendations/', views.recommendations, name='recommendations'),
 ]
